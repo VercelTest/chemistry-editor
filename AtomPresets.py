d9570deb8de078f436a04cd8ade2191a695a6bb7
Jeffrey Ngetich <jeffrey.ngetich@outlook.com>
2025-02-20 10:59:00 -0700

    added tests

---
+++ AtomPresets.py
@@ -0,0 +1,5 @@
+import pygame
+AtomImages = {
+    "Hydrogen" : pygame.transform.scale(pygame.image.load('Assets/AtomImages/Hydrogen.webp'), (50,50)),
+    "Oxygen" : pygame.transform.scale(pygame.image.load('Assets/AtomImages/Oxygen.webp'), (50,50))
+}
--- MainRender.py
+++ MainRender.py
@@ -1,34 +1,73 @@
 import pygame
-from BallPhysics import *
+from AtomPhysics import *
 from sys import exit
+from AtomPresets import AtomImages
 
+# init variables
+SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 800
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-# Title
+Vector2 = pygame.math.Vector2
+
+# Window Title
 pygame.display.set_caption("Chemistry Editor")
 
 # Fonts
 TitleFont = pygame.font.Font('Fonts/Interphases Regular.ttf', 40)
 
-# Render
-Information = TitleFont.render("Interphases", True, 'white')
-text_rect = Information.get_rect(center=(800/2, 30))
+# Assets
+Information = TitleFont.render("Hi this is text", True, 'white')
+text_rect = Information.get_rect(center=(SCREEN_WIDTH/2, 30)) # center text
 
-ball = pygame.image.load('AtomImages/test 2.webp')
-ball = pygame.transform.scale(ball, (50,50))
+AddButtonImage = pygame.image.load('Assets/Buttons/Add Button.png').convert_alpha()
 
+# Classes
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (scale, scale))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False 
+
+    def draw(self):
+        action = False
+
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+        
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+class Atom():
+    def __init__(self):
+        return 'not yet!!!!'
+
+# Buttons
+AddButton = Button(SCREEN_WIDTH - 75, SCREEN_HEIGHT - 75, AddButtonImage, 75)
+
+# Renderer
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
-        if event.type == pygame.MOUSEMOTION:
-                print(event)
-                
-    screen.blit(ball, (0,0))
+
+    screen.fill((0, 15, 38))
+
     screen.blit(Information, text_rect)
+    if AddButton.draw():
+        print('START')
 
     pygame.display.update()
     # frame rate
